6cd275da1d01edae82808038a4632173c59fc523
nino3305 <jack25845655@gmail.com>
2025-08-26 06:59:46 +0800

    Update bot.py

--- alice/bot.py
+++ alice/bot.py
@@ -67,11 +67,11 @@ async def on_typing(channel, user, when):
     if isinstance(channel, discord.TextChannel):
         await channel.send(
             f"💬 {user.display_name} 正在打字...",
-            delete_after=5,  # 5 秒後自動刪除
-            allowed_mentions=discord.AllowedMentions.none()
+            delete_after=5  # 5 秒後自動刪除
         )
 
 bot.run(TOKEN)
+
 
 
 
