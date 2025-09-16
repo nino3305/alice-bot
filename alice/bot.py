80f1b9d38245825d5d8b6797ad548a5a6953e8cb
nino3305 <jack25845655@gmail.com>
2025-09-16 23:44:43 +0800

    Update bot.py

--- alice/bot.py
+++ alice/bot.py
@@ -30,7 +30,7 @@ async def on_voice_state_update(member, before, after):
         # 成員加入語音頻道
         channel = bot.get_channel(TARGET_CHANNEL_ID)
         if channel:
-            await channel.send(f":o:  {member.display_name} 加入了語音頻道 {after.channel.name}")
+            await channel.send(f":white_check_mark:   {member.display_name} 加入了語音頻道 {after.channel.name}")
     elif before.channel is not None and after.channel is None:
         # 成員離開語音頻道
         channel = bot.get_channel(TARGET_CHANNEL_ID)
@@ -40,6 +40,7 @@ async def on_voice_state_update(member, before, after):
 
 
 bot.run(TOKEN)
+
 
 
 
